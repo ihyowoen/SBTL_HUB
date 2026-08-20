a3fde83ff25d652bf5217fedfb53e4a4478d97e3
ihyowoen <hyowoen@hotmail.com>
2026-08-20 23:45:49 +0900

    test: point Stage C revise R3 validator at exact4k transport

--- validation_scripts/tests/test_stage_c_revise_r1_rescue7_validator_fixture.py
+++ validation_scripts/tests/test_stage_c_revise_r1_rescue7_validator_fixture.py
@@ -1,1 +1,16 @@
-from validation_scripts.tests.test_stage_c_revise_r1_0_7c_rescue7_validator_fixture import *  # noqa: F401,F403
+from validation_scripts.tests import test_stage_c_revise_r1_0_7c_rescue7_validator_fixture as _base
+
+_base.CHUNK_NAMES = [
+    "stage_c_revise_r1_rescue7_R3_exact4k_00.txt",
+    "stage_c_revise_r1_rescue7_R3_exact4k_01.txt",
+    "stage_c_revise_r1_rescue7_R3_exact4k_02.txt",
+    "stage_c_revise_r1_rescue7_R3_exact4k_03.txt",
+    "stage_c_revise_r1_rescue7_R3_exact4k_04.txt",
+    "stage_c_revise_r1_rescue7_R3_exact4k_05.txt",
+    "stage_c_revise_r1_rescue7_R3_exact4k_06.txt",
+    "stage_c_revise_r1_rescue7_R3_exact4k_07.txt",
+    "stage_c_revise_r1_rescue7_R3_exact4k_08.txt",
+    "stage_c_revise_r1_rescue7_R3_exact4k_09.txt",
+]
+
+TestStageCReviseR107CRescue7ValidatorFixture = _base.TestStageCReviseR107CRescue7ValidatorFixture

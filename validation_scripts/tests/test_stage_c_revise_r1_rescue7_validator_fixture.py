7b704b953ab00e188f89b268e344bb4566036342
ihyowoen <hyowoen@hotmail.com>
2026-08-20 23:51:11 +0900

    test: use verified exact Stage C revise R3 transport

--- validation_scripts/tests/test_stage_c_revise_r1_rescue7_validator_fixture.py
+++ validation_scripts/tests/test_stage_c_revise_r1_rescue7_validator_fixture.py
@@ -1,15 +1,16 @@
 from validation_scripts.tests import test_stage_c_revise_r1_0_7c_rescue7_validator_fixture as _base
 
 _base.CHUNK_NAMES = [
-    "stage_c_revise_r1_rescue7_R3_exact4k_00.txt",
-    "stage_c_revise_r1_rescue7_R3_exact4k_01.txt",
-    "stage_c_revise_r1_rescue7_R3_exact4k_02.txt",
-    "stage_c_revise_r1_rescue7_R3_exact4k_03.txt",
-    "stage_c_revise_r1_rescue7_R3_exact4k_04.txt",
-    "stage_c_revise_r1_rescue7_R3_exact4k_05.txt",
+    "stage_c_revise_r1_R3_4k_00.txt",
+    "stage_c_revise_r1_R3_4k_01.txt",
+    "stage_c_revise_r1_R3_4k_02.txt",
+    "stage_c_revise_r1_R3_4k_03.txt",
+    "stage_c_revise_r1_R3_4k_04.txt",
+    "stage_c_revise_r1_R3_4k_05.txt",
     "stage_c_revise_r1_rescue7_R3_exact4k_06.txt",
     "stage_c_revise_r1_rescue7_R3_exact4k_07.txt",
-    "stage_c_revise_r1_rescue7_R3_exact4k_08.txt",
+    "stage_c_revise_r1_rescue7_R3_exact2k_08a.txt",
+    "stage_c_revise_r1_rescue7_R3_exact2k_08b.txt",
     "stage_c_revise_r1_rescue7_R3_exact4k_09.txt",
 ]
 
